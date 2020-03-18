131c4b18fa1f4f4f207b1ba53081ff187e2540ef
wallace128 <nathanwallace512@gmail.com>
2020-03-18 11:06:57 -0600

    my first commit

---
+++ starter-code/mock-data.js
@@ -0,0 +1,248 @@
+let mock = [{
+    id: 1,
+    name: "McDonalds",
+    country: "Provo",
+    image: 'https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/4578/38854427451_67b5d3af45_m.jpg'
+  },
+  {
+    id: 2,
+    name: "Starbucks",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://farm6.staticflickr.com/5823/22224416615_ab9c21865a_m.jpg"
+  },
+  {
+    id: 3,
+    name: "Subway",
+    country: "Provo",
+    image: 'https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/3228/2836141985_39f4d3ba5f_m.jpg'
+  },
+  {
+    id: 4,
+    name: "Wendy's",
+    country: "Provo",
+    image: 'https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/8232/8486645487_8959989262_m.jpg'
+  },
+  {
+    id: 5,
+    name: "Burger King",
+    country: "Salt Lake",
+    image: 'https://api.creativecommons.engineering/t/600/https://farm8.staticflickr.com/7302/12801670223_9aafcd6635_m.jpg'
+
+  },
+  {
+    id: 6,
+    name: "Taco Bell",
+    country: "Orem",
+    image: 'https://api.creativecommons.engineering/t/600/https://farm8.staticflickr.com/7459/14088601201_b598110d9d_m.jpg'
+  },
+  {
+    id: 7,
+    name: "Dunkin' Doughnuts",
+    country: "Orem",
+    image: 'https://farm6.staticflickr.com/5563/14883184373_df45831120_b.jpg'
+  },
+  {
+    id: 8,
+    name: "Chick-fil-a",
+    country: "Salt Lake",
+    image: 'https://api.creativecommons.engineering/t/600/https://farm1.staticflickr.com/385/19286629311_ae60c4117d_m.jpg'
+  },
+  {
+    id: 9,
+    name: "Pizza Hut",
+    country: "Orem",
+    image: 'https://api.creativecommons.engineering/t/600/https://farm3.staticflickr.com/2479/3717306949_f9d18991c0_m.jpg'
+
+  },
+  {
+    id: 10,
+    name: "Dominos",
+    country: "Orem",
+    image: 'https://api.creativecommons.engineering/t/600/https://farm3.staticflickr.com/2262/2522541678_baa2e31fb4_m.jpg'
+  },
+  {
+    id: 11,
+    name: "Little Cesears",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://farm4.staticflickr.com/3862/14889232677_de7ea80855_m.jpg"
+  },
+  {
+    id: 12,
+    name: "Panera Bread",
+    country: "Salt Lake",
+    image: "https://api.creativecommons.engineering/t/600/https://farm3.staticflickr.com/2915/14661305453_8b7a13ef93_m.jpg"
+  },
+  {
+    id: 13,
+    name: "Sonic Drive-in",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://farm3.staticflickr.com/2819/13755076313_9b5d25af1b_m.jpg"
+  },
+  {
+    id: 14,
+    name: "KFC",
+    country: "Mexico",
+    image: "https://api.creativecommons.engineering/t/600/https://farm5.staticflickr.com/4121/4888007513_978bd6d95d_m.jpg"
+  },
+  {
+    id: 15,
+    name: "Chipotle",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://farm4.staticflickr.com/3850/14725819420_998e7b1572_m.jpg"
+  },
+  {
+    id: 16,
+    name: "Carl's JR",
+    country: "Mexico",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/3221/3039167134_7d07b0c5db_m.jpg"
+  },
+  {
+    id: 17,
+    name: "Zupas",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/3119/2294169315_fb56a6c87d_m.jpg"
+  },
+  {
+    id: 18,
+    name: "Dairy Queen",
+    country: "Salt Lake",
+    image: "https://api.creativecommons.engineering/t/600/https://farm3.staticflickr.com/2930/14100955515_48aff8e2d7_m.jpg"
+  },
+  {
+    id: 19,
+    name: "Arby's",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/2271/2525314643_2ed4f98c45_m.jpg"
+  },
+  {
+    id: 20,
+    name: "Jack in the box",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/1393/1155036223_506d6d6284_m.jpg"
+  },
+  {
+    id: 21,
+    name: "Popeyes",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://farm6.staticflickr.com/5652/22689867160_0e43498f6c_m.jpg"
+  },
+  {
+    id: 22,
+    name: "Papa Johns",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://mir-s3-cdn-cf.behance.net/project_modules/1400/127d0e51819355.5bf25d4e36d15.png"
+  },
+  {
+    id: 23,
+    name: "Cubbies",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/8333/8125837139_34a99e8371_m.jpg"
+  },
+  {
+    id: 24,
+    name: "Panda Express",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/5682/30423386031_a50eaef3a8_m.jpg"
+  },
+  {
+    id: 25,
+    name: "Cafe Rio",
+    country: "Mexico",
+    image: "https://api.creativecommons.engineering/t/600/https://farm1.staticflickr.com/166/390753302_170f2dbc6f_m.jpg"
+  },
+  {
+    id: 26,
+    name: "Costa Vida",
+    country: "Salt Lake",
+    image: "https://api.creativecommons.engineering/t/600/https://mir-s3-cdn-cf.behance.net/project_modules/disp/1a541241350283.5606f916f321a.jpg"
+  },
+  {
+    id: 27,
+    name: "Jimmy Johns",
+    country: "Salt Lake",
+    image: "https://api.creativecommons.engineering/t/600/https://upload.wikimedia.org/wikipedia/commons/e/e3/Jimmy_John%27s_Gourmet_Sandwiches_-_panoramio.jpg"
+
+  },
+  {
+    id: 28,
+    name: "Zaxby's",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/3356/3296827780_7cf27e8b37_m.jpg"
+  },
+  {
+    id: 29,
+    name: "Five Guys",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://farm3.staticflickr.com/2035/2379586847_3b768040be_m.jpg"
+  },
+  {
+    id: 30,
+    name: "Steak n Shake",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/1006/1339351120_dec30d03d5_m.jpg"
+
+  },
+  {
+    id: 31,
+    name: "Papa Murphy's",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://upload.wikimedia.org/wikipedia/commons/c/cc/Papa_Murphy%27s_-_panoramio.jpg"
+  },
+  {
+    id: 32,
+    name: "El Pollo Loco",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://farm9.staticflickr.com/8222/28423470600_76251d6907_m.jpg"
+  },
+  {
+    id: 33,
+    name: "Del Taco",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://mir-s3-cdn-cf.behance.net/project_modules/disp/9874f329596451.56056053cb579.png"
+  },
+  {
+    id: 34,
+    name: "Taco Time",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/5337/8926260978_e855086c93_m.jpg"
+
+  },
+  {
+    id: 35,
+    name: "Firehouse Subs",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://upload.wikimedia.org/wikipedia/commons/3/37/Firehouse_Subs_-_panoramio.jpg"
+  },
+  {
+    id: 36,
+    name: "in-N-out Burger",
+    country: "Salt Lake",
+    image: "avocado-oil.jpg"
+  },
+  {
+    id: 37,
+    name: "Baskin Robins",
+    country: "Salt Lake",
+    image: "https://api.creativecommons.engineering/t/600/https://farm1.staticflickr.com/301/18821018359_49ee248096_m.jpg"
+  },
+  {
+    id: 38,
+    name: "Einstien Bagels",
+    country: "Mexico",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/176/367574633_bdaaeb4c1d_m.jpg"
+  },
+  {
+    id: 39,
+    name: "Red Robin",
+    country: "Provo",
+    image: "https://api.creativecommons.engineering/t/600/https://farm6.staticflickr.com/5608/15477433375_14f0cfa1ab_m.jpg"
+  },
+  {
+    id: 40,
+    name: "Jamba Juice",
+    country: "Orem",
+    image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/5591/30137447231_a4d83653e2_m.jpg"
+  },
+]
+
+export default mock;
